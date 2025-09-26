a0c2310e5676dd2f4f0386665029bb94c3c9757a
Sanskriti <sanskriti.gupta@yendigital.com>
2025-09-26 16:19:59 +0530

    commit for connector project.

---
+++ Bin/Release/Publish/App_Code/Global.asax.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web;
+using System.Web.SessionState;
+
+namespace GComm.Integrations.QBD 
+{
+	/// <summary>
+	/// Summary description for Global.
+	/// </summary>
+	public class Global : System.Web.HttpApplication
+	{
+	}
+}
+
